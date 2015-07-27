903d43f8c22affed239469c7e5c0136e3348342d
Dennis Hoer <dennis.hoer@pearson.com>
2015-07-27 16:30:36 -0700

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
 description 'Sets headless screen resolution on Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '1.0.0'
 
 supports 'windows'
 
