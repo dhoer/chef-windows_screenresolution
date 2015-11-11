2f5d6ec98ee05056b415a0296cb0d756aef2a834
Dennis Hoer <dennis.hoer@pearson.com>
2015-11-10 18:44:13 -0700

    Add source and issues urls to metadata

--- metadata.rb
+++ metadata.rb
@@ -10,3 +10,6 @@ supports 'windows'
 
 depends 'windows_home', '~> 1.0'
 depends 'windows_autologin', '~> 1.0'
+
+source_url 'https://github.com/dhoer/chef-windows_screenresolution' if respond_to?(:source_url)
+issues_url 'https://github.com/dhoer/chef-windows_screenresolution/issues' if respond_to?(:issues_url)
