e9e2b284fb38730aa59c3dd7cec0a0805a71c888
Dennis Hoer <dennis.hoer@pearson.com>
2016-12-22 00:34:21 -0700

    Tweak selenium integration test

--- test/integration/default/rspec/firefox_spec.rb
+++ test/integration/default/rspec/firefox_spec.rb
@@ -2,10 +2,7 @@ require 'rspec_helper'
 
 describe 'Firefox Grid' do
   before(:all) do
-    Selenium::WebDriver::Firefox.path = 'C:\Program Files (x86)\Mozilla Firefox\firefox.exe'
-    profile = Selenium::WebDriver::Firefox::Profile.new
-    profile.proxy = Selenium::WebDriver::Proxy.new(http: nil)
-    @selenium = Selenium::WebDriver.for :firefox, profile: profile
+    @selenium = Selenium::WebDriver.for :firefox
   end
 
   after(:all) do
