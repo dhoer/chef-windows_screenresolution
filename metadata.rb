753d17f0f4ec7936609652d6d4091a24be01f8f6
Dennis Hoer <dennis.hoer@pearson.com>
2017-02-03 16:13:43 -0700

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Sets headless screen resolution on Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/dhoer/chef-windows_screenresolution' if respond_to?(:source_url)
 issues_url 'https://github.com/dhoer/chef-windows_screenresolution/issues' if respond_to?(:issues_url)
-version '1.1.0'
+version '2.0.0'
 
 supports 'windows'
 
